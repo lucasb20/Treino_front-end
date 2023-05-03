36c6cb620c318679b524bb94a4f83e1d02816b1f
lucasb20 <lr359508@gmail.com>
2023-05-02 21:58:48 -0300

    Iniciando um site verde.

---
+++ exercícios/desafio_android/script.js
@@ -0,0 +1,9 @@
+function enter(a){
+    let ancora = document.getElementsByTagName('a')
+    ancora[a].setAttribute('class','menucheck')
+}
+
+function sair(a){
+    let ancora = document.getElementsByTagName('a')
+    ancora[a].setAttribute('class','none')
+}
